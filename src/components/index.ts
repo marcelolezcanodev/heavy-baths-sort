22b2044229c5169b17b63fa81391974b7f151b67
chelolezcano971 <marcelo.lezcano.dev@gmail.com>
2024-10-15 23:51:13 -0300

    Minimal CRM with Refine Course

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,19 @@
-export { Header } from "./header";
+import UpcomingEvents from "./home/upcoming-events";
+import DealsChart from "./home/deals-chart";
+import UpcomingEventsSkeleton from "./skeleton/upcoming-events";
+import KanbanColumnSkeleton from "./skeleton/kanban";
+import AccordionHeaderSkeleton from "./skeleton/accordion-header";
+import LatestActivitiesSkeleton from "./skeleton/latest-activities";
+import ProjectCardSkeleton from "./skeleton/project-card";
+import DashboardTotalCountCard from "./home/total-count-card";
+
+export {
+  UpcomingEvents,
+  DealsChart,
+  UpcomingEventsSkeleton,
+  AccordionHeaderSkeleton,
+  KanbanColumnSkeleton,
+  LatestActivitiesSkeleton,
+  ProjectCardSkeleton,
+  DashboardTotalCountCard,
+};
